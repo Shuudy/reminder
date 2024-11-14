75e032c353e77b82117a3112837b290016830532
Shuudy <ancieux.jerome059@gmail.com>
2024-11-14 14:58:20 +0100

    feat(models): add relationships between models

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Album extends Model
 {
     //
+
+    public function photos()
+    {
+        return $this->hasMany(Photo::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     //
+    public function album() 
+    {
+        return $this->belongsTo(Album::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
